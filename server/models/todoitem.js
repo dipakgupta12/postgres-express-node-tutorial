0d5dde61182cda9a65c86fd25ab40875a18915bf
dipakgupta <dipakgupta12121@gmail.com>
2021-05-03 18:21:07 +0530

    added new filed complete_status in TodoItem

--- server/models/todoitem.js
+++ server/models/todoitem.js
@@ -1,18 +1,20 @@
 module.exports = (sequelize, DataTypes) => {
-  const TodoItem = sequelize.define('TodoItem', {
+  const TodoItem = sequelize.define("TodoItem", {
     content: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    complete: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    }
+    complete_status: {
+      type: DataTypes.ENUM,
+      allowNull: false,
+      values: ["pending", "done", "inprogress"],
+      defaultValue: "pending",
+    },
   });
   TodoItem.associate = (models) => {
     TodoItem.belongsTo(models.Todo, {
-      foreignKey: 'todoId',
-      onDelete: 'CASCADE',
+      foreignKey: "todoId",
+      onDelete: "CASCADE",
     });
   };
   return TodoItem;
